fb81f3b759acaf7fe6228b852b52feb3f228b409
Krisna Firdaus <krisnafirdaus@gmail.com>
2026-01-17 10:20:37 +0700

    add env

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -2,6 +2,8 @@ import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
 export function Welcome() {
+  console.log(import.meta.env.VITE_KEY);
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
